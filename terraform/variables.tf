1e258a52f9d9a2ef2494ab1ad44be72a6073b845
Luixh-R <127801162+Luixh-R@users.noreply.github.com>
2026-01-11 01:52:09 +0000

    feat: add VPC and public subnets for EKS networking

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,15 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the vpc"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
