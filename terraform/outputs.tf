06c3f5dfde5daa0fcd8b31a59f85e5f0d66515bf
Luixh-R <127801162+Luixh-R@users.noreply.github.com>
2026-01-13 01:14:22 +0000

    feat: add EKS control plane with IAM role

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,11 @@ output "public_subnet_ids" {
 output "ecr_repository_url" {
  value = aws_ecr_repository.app.repository_url
 }
+
+output "eks_cluster_name" {
+ value = aws_eks_cluster.this.name
+}
+
+output "eks_cluster_endpoint" {
+ value = aws_eks_cluster.this.endpoint
+}
