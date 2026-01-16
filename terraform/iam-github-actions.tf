53079457221c7b3fe6b324ff51562f19c9021751
Luixh-R <127801162+Luixh-R@users.noreply.github.com>
2026-01-16 01:16:06 +0000

    fix: correct GitHub OIDC client_id_list audience typo

--- terraform/iam-github-actions.tf
+++ terraform/iam-github-actions.tf
@@ -2,7 +2,7 @@ resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 
   client_id_list = [
-    "sts.amazon.com"
+    "sts.amazonaws.com"
   ]
 
   thumbprint_list = [
